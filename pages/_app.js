4dd44a3e93a38de4c68b258de797a0d695925196
kamshushi <ahmed.elkamshoushy7@yahoo.com>
2021-11-15 22:21:04 +0200

    2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { supabase } from '../utils/supabaseClient';
+import { Auth } from '@supabase/ui';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth.UserContextProvider supabaseClient={supabase}>
+      <Component {...pageProps} />
+    </Auth.UserContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,124 +1,24 @@
-import Link from 'next/link';
-import useSWR from 'swr';
-import { Auth, Card, Typography, Space, Button, Icon } from '@supabase/ui';
+import React from 'react';
 import { supabase } from '../utils/supabaseClient';
-import { useEffect, useState } from 'react';
 
-const fetcher = (url, token) =>
-  fetch(url, {
-    method: 'GET',
-    headers: new Headers({ 'Content-Type': 'application/json', token }),
-    credentials: 'same-origin',
-  }).then((res) => res.json());
-
-const Index = () => {
-  const { user, session } = Auth.useUser();
-  const { data, error } = useSWR(
-    session ? ['/api/getUser', session.access_token] : null,
-    fetcher
-  );
-  const [authView, setAuthView] = useState('sign_in');
-
-  useEffect(() => {
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      (event, session) => {
-        // if (event === 'PASSWORD_RECOVERY') setAuthView('forgotten_password');
-        // if (event === 'USER_UPDATED')
-        //   setTimeout(() => setAuthView('sign_in'), 1000);
-
-        // Send session to /api/auth route to set the auth cookie.
-        // NOTE: this is only needed if you're doing SSR (getServerSideProps)!
-        fetch('/api/auth', {
-          method: 'POST',
-          headers: new Headers({ 'Content-Type': 'application/json' }),
-          credentials: 'same-origin',
-          body: JSON.stringify({ event, session }),
-        }).then((res) => res.json());
-      }
-    );
-
-    return () => {
-      authListener.unsubscribe();
-    };
-  }, []);
-
-  const View = () => {
-    if (!user)
-      return (
-        <Space direction="vertical" size={8}>
-          <div>
-            <img
-              src="https://app.supabase.io/img/supabase-light.svg"
-              width="96"
-            />
-            <Typography.Title level={3}>
-              Welcome to Supabase Auth
-            </Typography.Title>
-          </div>
-          <Auth
-            supabaseClient={supabase}
-            providers={['google', 'github', 'twitter', 'facebook']}
-            view={authView}
-            socialLayout="horizontal"
-            socialButtonSize="xlarge"
-          />
-        </Space>
-      );
-
-    return (
-      <Space direction="vertical" size={6}>
-        {authView === 'forgotten_password' && (
-          <Auth.UpdatePassword supabaseClient={supabase} />
-        )}
-        {user && (
-          <>
-            <Typography.Text>You're signed in</Typography.Text>
-            <Typography.Text strong>Email: {user.email}</Typography.Text>
-
-            <Button
-              icon={<Icon type="LogOut" />}
-              type="outline"
-              onClick={() => supabase.auth.signOut()}
-            >
-              Log out
-            </Button>
-            {error && (
-              <Typography.Text type="danger">
-                Failed to fetch user!
-              </Typography.Text>
-            )}
-            {data && !error ? (
-              <>
-                <Typography.Text type="success">
-                  User data retrieved server-side (in API route):
-                </Typography.Text>
-
-                <Typography.Text>
-                  <pre>{JSON.stringify(data, null, 2)}</pre>
-                </Typography.Text>
-              </>
-            ) : (
-              <div>Loading...</div>
-            )}
-
-            <Typography.Text>
-              <Link href="/profile">
-                <a>SSR example with getServerSideProps</a>
-              </Link>
-            </Typography.Text>
-          </>
-        )}
-      </Space>
-    );
-  };
-
-  return (
-    <div style={{ maxWidth: '420px', margin: '96px auto' }}>
-      <Card>
-        <View />
-      </Card>
-    </div>
-  );
+const Stories = (props) => {
+  const { user, stories } = props;
+  console.log(stories);
+  return <div></div>;
 };
 
-export default Index;
+export default Stories;
+
+export async function getServerSideProps({ req }) {
+  const response = await supabase.auth.api.getUserByCookie(req);
+  const { user } = response;
+  if (!user) {
+    // If no user, redirect to index.
+    return { props: {}, redirect: { destination: '/login', permanent: false } };
+  }
+
+  const storiesResponse = await supabase.from('stories').select('*');
+
+  // If there is a user, return it.
+  return { props: { user, stories: storiesResponse.data } };
+}
