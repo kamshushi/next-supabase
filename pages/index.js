8412d8e461a10e30ff6b5bb7060307f62dcccf3c
kamshushi <ahmed.elkamshoushy7@yahoo.com>
2021-11-16 23:04:37 +0200

    4

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { supabase } from '../utils/supabaseClient';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 const Stories = (props) => {
+  dayjs.extend(relativeTime);
   const { user, stories } = props;
   console.log(stories);
   return (
@@ -10,72 +13,56 @@ const Stories = (props) => {
         <div className="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
           <h2 className="text-2xl font-extrabold text-gray-900">Stories</h2>
 
-          <div className="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
-            <div className="group relative">
-              <div className="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg"
-                  alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
-                  className="w-full h-full object-center object-cover"
-                />
-              </div>
-              <h3 className="mt-6 text-sm text-gray-500">
-                <a href="#">
-                  <span className="absolute inset-0"></span>
-                  Desk and Office
-                </a>
-              </h3>
-              <p className="text-base font-semibold text-gray-900">
-                Work from home accessories
-              </p>
-            </div>
+          <div className="mt-6 lg:grid md:grid md:grid-cols-2 lg:grid-cols-3 gap-y-11 lg:gap-x-6">
+            {stories.map((story) => {
+              const {
+                id,
+                title,
+                fulltext,
+                summary,
+                source,
+                published_at,
+                url,
+                author,
+                created_at,
+              } = story;
 
-            <div className="group relative">
-              <div class="max-w-sm w-full lg:max-w-full lg:flex ">
-                <div class="border-r  rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                  <div class="mb-8">
-                    <div class="text-gray-900 font-bold text-xl mb-2">
-                      Can coffee make you a better developer?
+              return (
+                <div key={id} className="mb-12 group ">
+                  <div className="m-auto max-w-sm rounded overflow-hidden shadow-lg">
+                    <div className="px-6 py-4">
+                      <div className="font-bold text-xl mb-5 hover:text-blue-500">
+                        <a target="_blank" href={url || '#'}>
+                          {title || 'Default Title for the story'}
+                        </a>
+                      </div>
+                      <p className="text-gray-700 text-base">
+                        {/* // TODO: fulltext */}
+                        {summary ||
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et condimentum sem, luctus convallis nisi. Aenean maximus lectus arcu, at pulvinar nibh condimentum eget. Sed a sem a metus eleifend lobortis. Cras vulputate id ante sed vulputate. Pellentesque imperdiet tempus dapibus. Phasellus euismod imperdiet arcu, eu suscipit augue euismod ultrices'}
+                      </p>
+                      <p className="my-5 text-gray-700 text-base">
+                        Source:&nbsp;&nbsp;
+                        <a
+                          className="underline text-blue-500 hover:text-blue-400"
+                          target="_blank"
+                          href={`https://${source || 'https://www.google.com'}`}
+                        >{`${source || 'www.google.com'}`}</a>
+                      </p>
                     </div>
-                    <p class="text-gray-700 text-base">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                      exercitationem praesentium nihil.
-                    </p>
-                  </div>
-                  <div class="flex items-center">
-                    <div class="text-sm">
-                      <p class="text-gray-900 leading-none">Jonathan Reinink</p>
-                      <p class="text-gray-600">Aug 18</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div className="group relative">
-              <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                <div class="px-6 py-4">
-                  <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                  <p class="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                    exercitationem praesentium nihil.
-                  </p>
+                    <div className="text-sm px-6 pt-4 pb-2">
+                      <p className="text-gray-900 leading-none">
+                        {author || 'John Doe'}
+                      </p>
+                      <p className="text-gray-600">
+                        {dayjs(created_at).fromNow() || 'Default Date'}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <div class="px-6 pt-4 pb-2">
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #photography
-                  </span>
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #travel
-                  </span>
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #winter
-                  </span>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -93,7 +80,11 @@ export async function getServerSideProps({ req }) {
   //   return { props: {}, redirect: { destination: '/login', permanent: false } };
   // }
 
-  const storiesResponse = await supabase.from('stories').select('*');
+  const storiesResponse = await supabase
+    .from('stories')
+    .select('*')
+    .order('created_at', { ascending: false })
+    .limit(100);
 
   // If there is a user, return it.
   return { props: { user, stories: storiesResponse.data } };
